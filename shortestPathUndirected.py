afe4b26ca6d1a01bac632268d46e4fa3000e0ab0
Pauline <pauline@Mimolette.(none)>
2012-12-09 00:09:58 +0100

    script qui fait le projet de A a Z, a tester sur gros graphe

--- shortestPathUndirected.py
+++ shortestPathUndirected.py
@@ -16,7 +16,7 @@ import pydot
 
 
 #-----------------------------------------------------------------------------------------------#
-#	  		  Conversion reads -> graphe de De Bruijn				#
+#--------------------- Etape 1 : Conversion reads -> graphe de De Bruijn -----------------------#
 #-----------------------------------------------------------------------------------------------#
 #Le fichier donne en parametre 1 contient une serie de reads au format fasta (extension .fa)
 fichier_reads=sys.argv[1]
@@ -44,7 +44,7 @@ fichier_graphe=str(fichier_reads).replace(".fa",".graph")
 
 
 #-----------------------------------------------------------------------------------------------#
-#	  Conversion graphe de De Bruijn -> graphe dirige sous forme de liste adjacente		#
+#-- Etape 2 : Conversion graphe de De Bruijn -> graphe dirige sous forme de liste adjacente ----#
 #-----------------------------------------------------------------------------------------------#
 #On ouvre le fichier fichier_graphe en lecture
 try:                     		
@@ -55,7 +55,7 @@ except IOError, e:
 #on cree la variable listAdj qui contiendra le nom du fichier sortie du script directeGraph.py
 listAdj="listeAdj.txt"
 
-#On execute le script python directedGraph.py qui transforme le graph de De Bruijn en graphe dirige (liste adjacente)
+#On execute le script python directedGraph.py qui transforme le graphe de De Bruijn en graphe dirige (liste adjacente)
 os.system("python directedGraph.py "+str(fichier_graphe)+" "+str(listAdj))
 
 #On ouvre le fichier listAdj  en lecture
@@ -66,57 +66,10 @@ except IOError, e:
 
 
 #-----------------------------------------------------------------------------------------------#
-#				Bac a sable : dessins de graphes				#
+#-------------------------------- Etape 3 : Lecture du graphe ----------------------------------#
 #-----------------------------------------------------------------------------------------------#
-#graph = pydot.Dot(graph_type='graph')
+#Cette partie permet de lire le graphe sous forme de liste adjacente, et d'en stocker les informations (noeuds et aretes)
 
-#for i in range(3):
-#	edge = pydot.Edge("king", "lord%d" % i)
-#	graph.add_edge(edge)
-#vassal_num = 0
-#for i in range(3):
-    # we create new edges, now between our previous lords and the new vassals
-    # let us create two vassals for each lord
-#    for j in range(2):
-#        edge = pydot.Edge("lord%d" % i, "vassal%d" % vassal_num)
-#        graph.add_edge(edge)
-#        vassal_num += 1
-
-# ok, we are set, let's save our graph into a file
-#graph.write_png('example1_graph.png')
-
-#Exemple 2
-#graph = pydot.Dot(graph_type='digraph')
-
-# creating nodes is as simple as creating edges!
-#node_a = pydot.Node("Node A", style="filled", fillcolor="#976806")
-#node_b = pydot.Node("Node B", style="filled", fillcolor="#976826")
-#node_c = pydot.Node("Node C", style="filled", fillcolor="#976846")
-#node_d = pydot.Node("Node D", style="filled", fillcolor="#976866")
-
-#ok, now we add the nodes to the graph
-#graph.add_node(node_a)
-#graph.add_node(node_b)
-#graph.add_node(node_c)
-#graph.add_node(node_d)
-
-# and finally we create the edges
-# to keep it short, I'll be adding the edge automatically to the graph instead
-# of keeping a reference to it in a variable
-
-#graph.add_edge(pydot.Edge(node_b, node_c))
-#graph.add_edge(pydot.Edge(node_c, node_d))
-# but, let's make this last edge special, yes?
-#graph.add_edge(pydot.Edge(node_d, node_a, label="and back we go again", labelfontcolor="#009933", fontsize="10.0", color="blue"))
-
-# and we are done
-#graph.write_png('example2_graph.png')
-
-# this is too good to be true!
-
-#-----------------------------------------------------------------------------------------------#
-#------------------------------ Partie 1 : Lecture du fichier ----------------------------------#
-#-----------------------------------------------------------------------------------------------#
 #On initialise la liste nodes qui contiendra la liste des noeuds du graphe
 nodes=list()
 
@@ -130,7 +83,6 @@ l=fic_listAdj.readline()
 while l:
 	#On split notre chaine de caractere en une liste liste_fichier de deux elements ie deux noeuds (un noeud pere et un noeud fils) 
 	liste_fichier = l.split()
-	print liste_fichier
 	if not nodes.__contains__(liste_fichier[0]):
 		nodes.append(liste_fichier[0])
 	if not nodes.__contains__(liste_fichier[1]) :
@@ -146,26 +98,56 @@ while l:
 	#On lit la ligne suivante du fichier
 	l=fic_listAdj.readline()
 
+#On calcule le nombre total de noeuds dans le reseau
+nb_nodes=len(nodes)
+
+
 #-----------------------------------------------------------------------------------------------#
-#					 Dessin du graphe					#
+#---------------------------- Etape 4 : Dessin du graphe de depart -----------------------------#
 #-----------------------------------------------------------------------------------------------#
+#Cette partie permet de dessiner le graphe avant la ponderation des noeuds par la centralite
+
+#Declaration d'un objet graph de type pydot.Dot
 graph = pydot.Dot(graph_type='graph')
+
+#Declaration du dictionnaire graph_node, qui contiendra les noeuds du graphe a dessiner
 graph_node=dict()
+
+#Declaration du dictionnaire graph_edges, qui contiendra les aretes du graphe a dessiner
+graph_edges=list()
+
+#On parcourt la liste des noeuds du graphe, node, creee a l'etape precedente
 for node in nodes :
+	#On ajoute au dessin chacun des noeuds
 	V = pydot.Node(str(node), style="filled")
 	graph_node[node]=V
 	graph.add_node(V)
+
+#On parcourt la liste des aretes du graphe, node, creee a l'etape precedente
 for key in edges.keys() :
 	node1=graph_node[key]
-	node2=graph_node[edges[key][0]]
-	graph.add_edge(pydot.Edge(node1, node2))
-graph.write_png('test.png')
+	for elem in edges[key] :
+		node2=graph_node[elem]
+		#On veille a ne pas dupliquer les aretes
+		edgeF=str(node1)+"to"+str(node2)
+		edgeR=str(node2)+"to"+str(node1)
+		#Si l'arete n'a pas deja ete dessine dans l'autre sens
+		if not graph_edges.__contains__(edgeF) and not graph_edges.__contains__(edgeR) :
+			#On dessine l'arete consideree sur le graphe
+			graph_edges.append(edgeF)			
+			edgeF=pydot.Edge(node1, node2)
+			graph.add_edge(edgeF)
 
+#On dessine le graphe dans le fichier graphe_before.png
+graph.write_png('graphe_before.png')
 
 
 #------------------------------------------------------------------------------------------------------------------#
-#-------------- Partie 2 : Fonctions de recherche du plus court chemin via l'algorithme de Dijkstra ---------------#
+#--------------- Etape 5 : Fonctions de recherche du plus court chemin via l'algorithme de Dijkstra ---------------#
 #------------------------------------------------------------------------------------------------------------------#
+#Cette partie permet de calculer les plus courts chemin entre chaque couple de noeuds du graphe, par l'algo de Dijkstra
+
+#Fonction permettran de retourner l'element minimim d'une liste Q selon son poids dans un dict dist
 def ExtractMin(Q,dist) :
 	mini=Q[0]
 	for i in Q :
@@ -173,9 +155,10 @@ def ExtractMin(Q,dist) :
 			mini=i
 	return mini
 
-
+#Fonction qui prend en parametre le dictionnaire des noeuds d'un graphe, le dictionnaire des aretes, et un sommet de depart
+#et qui retourne dans un dictionnaire les plus courtes distances (ici, pas de ponderation des aretes) qui existent entre s et 
+#chacun des autres noeuds du graphe, selon l'algorithme de Dijsktra
 def dijkstra(nodes,edges,s) :
-
 	dist=dict()
 	Q=list()
 	for n in nodes :
@@ -191,25 +174,55 @@ def dijkstra(nodes,edges,s) :
 					dist[v]=dist[u]+1
 	return dist
 
-somme_dist=0
+#Suite d'instructions permettant d'appliquer la fonction dijkstra a tous les noeuds du graphe
 dico_final=dict()
 nodes1=list()
 for j in nodes:
-		nodes1.append(j)
+	nodes1.append(j)
 for i in nodes:
 	dico_final[i]=[]
-
 for i in nodes:
 	dist=dijkstra(nodes1,edges,i)
-	
 	for j in nodes:
 		nodes1.append(j)
 	for k in nodes:
 		dico_final[k].append(dist[k])
-		somme_dist+=dist[k]
 
-print dico_final
-print somme_dist
-	
-	
+
+#------------------------------------------------------------------------------------------------------------------#
+#-------------------------- Etape 6 : Calcul de la centralite de proximite de chaque noeud ------------------------#
+#------------------------------------------------------------------------------------------------------------------#
+#Cette partie permet de calculer la centralite associee a chacun des noeuds du graphe, selon la formule :
+#C(i)=(n-1)/[somme(d(i,j))] avec n = nombre total de noeuds du graphe, et d(i,j) = plus courte distance de i a j
+centrality=dict()
+for vertex in dico_final :
+	sum_dist=0
+	for dist in dico_final[vertex] :
+		sum_dist+=float(dist)
+	centrality[vertex]=float((nb_nodes-1)/sum_dist)
+
+#-----------------------------------------------------------------------------------------------------------------#
+#------------------------------------ Etape 7 : Dessin du graphe final -------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------#
+#Cette etape permet de dessiner le graphe avec une intensite de couleur proportionnelle aux centralites des noeuds
+graph = pydot.Dot(graph_type='graph')
+graph_node=dict()
+graph_edges=list()
+for node in nodes :
+	color="#"+"44"+str(int(centrality[node]*10000))
+	V = pydot.Node(str(node), style="filled",fillcolor=color)
+	graph_node[node]=V
+	graph.add_node(V)
+for key in edges.keys() :
+	node1=graph_node[key]
+	for elem in edges[key] :
+		node2=graph_node[elem]
+		edgeF=str(node1)+"to"+str(node2)
+		edgeR=str(node2)+"to"+str(node1)
+		if not graph_edges.__contains__(edgeF) and not graph_edges.__contains__(edgeR) :
+			graph_edges.append(edgeF)			
+			edgeF=pydot.Edge(node1, node2)
+			graph.add_edge(edgeF)
+graph.write_png('graphe_after.png')
+
 	
